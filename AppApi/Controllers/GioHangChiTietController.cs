4613fae56cc1eb3440dbaeb2200ec06d6c229396
DESKTOP-D0FH2T9\Hi <thanhttph21806@fpt.edu.vn>
2023-07-19 19:17:18 +0700

    API GioHang, GioHangCT, Anh, NhanVien

---
+++ AppApi/Controllers/GioHangChiTietController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppData.Data;
+using AppData.IAllrepository;
+using AppData.AllRepository;
+
+namespace AppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GioHangChiTietController : ControllerBase
+    {
+        private IAllrepositories<GioHangChiTiet> _Irepos;
+        private ShopDbContext context = new ShopDbContext();
+
+        public GioHangChiTietController()
+        {
+            Allrepositories<GioHangChiTiet> repos = new Allrepositories<GioHangChiTiet>(context, context.gioHangChiTiets);
+            _Irepos = repos;
+        }
+        [HttpGet("Get-All")]
+        public IEnumerable<GioHangChiTiet> Get()
+        {
+            return _Irepos.GetAllItem();
+        }
+
+        [HttpPost("Create")]
+        public bool Create(Guid idctsp, int soluong)
+        {
+            GioHangChiTiet ghct = new GioHangChiTiet();
+            ghct.IdCTSP = idctsp;
+            ghct.SoLuong = soluong;
+            return _Irepos.CreateItem(ghct);
+        }
+
+        [HttpPut("Update")]
+        public bool Update(Guid Id, Guid idctsp, int soluong)
+        {
+            GioHangChiTiet ghct = _Irepos.GetAllItem().First(p => p.Id == Id);
+            ghct.IdCTSP = idctsp;
+            ghct.SoLuong = soluong;
+            return _Irepos.UpdateItem(ghct);
+        }
+
+        [HttpDelete("Delete")]
+        public bool Delete(Guid id)
+        {
+            GioHangChiTiet ghct = _Irepos.GetAllItem().First(p => p.Id == id);
+            return _Irepos.DeleteItem(ghct);
+        }
+    }
+}
---
+++ AppApi/Controllers/GioHangController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppData.Data;
+using AppData.IAllrepository;
+using AppData.AllRepository;
+
+namespace AppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GioHangController : ControllerBase
+    {
+        private IAllrepositories<GioHang> _Irepos;
+        private ShopDbContext context = new ShopDbContext();
+
+        public GioHangController()
+        {
+            Allrepositories<GioHang> repos = new Allrepositories<GioHang>(context, context.gioHangs);
+            _Irepos = repos;
+        }
+        [HttpGet("Get-All")]
+        public IEnumerable<GioHang> Get()
+        {
+            return _Irepos.GetAllItem();
+        }
+
+        [HttpPost("Create")]
+        public bool Create(Guid IdKH, string mota, int trangthai)
+        {
+            GioHang gh = new GioHang();
+            gh.IdKH = IdKH;
+            gh.Mota = mota;
+            gh.TrangThai = trangthai;
+            return _Irepos.CreateItem(gh);
+        }
+
+        [HttpPut("Edit")]
+        public bool Update(Guid Id, Guid IdKH, string mota, int trangthai)
+        {
+            GioHang gh = _Irepos.GetAllItem().First(p => p.Id == Id);
+            gh.IdKH = IdKH;
+            gh.Mota = mota;
+            gh.TrangThai = trangthai;
+            return _Irepos.UpdateItem(gh);
+        }
+
+        [HttpDelete("Delete")]
+        public bool Delete(Guid id)
+        {
+            GioHang gh = _Irepos.GetAllItem().First(p => p.Id == id);
+            return _Irepos.DeleteItem(gh);
+        }
+    }
+}
