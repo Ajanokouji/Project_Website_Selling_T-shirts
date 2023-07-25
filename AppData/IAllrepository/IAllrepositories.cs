bcbfab2889a53379b6e1205f9b65f436d000ffcb
Ajanokouji <trihnph26922@fpt.edu.vn>
2023-07-25 14:33:11 +0700

    Repos Updated

--- AppData/IAllrepository/IAllrepositories.cs
+++ AppData/IAllrepository/IAllrepositories.cs
@@ -13,15 +13,6 @@ namespace AppData.IAllrepository
 		public bool CreateItem (T item);
 		public bool DeleteItem (T item);
 		public bool UpdateItem (T item);
-		public ChatLieu GetCLByID (Guid id);
-		public ChucVu GetCVByID (Guid id);
-		public GioHang GetGHByID (Guid id);
-		public HoaDon GetHDByID (Guid id);
-		public KhachHang GetKHByID (Guid id);
-		public KichCo GetKCByID (Guid id);
-		public Loai GetLoaiByID (Guid id);
-		public MauSac GetMSByID (Guid id);
-		public NhanVien GetNVByID (Guid id);
-		public SanPham GetSPByID (Guid id);
+		public T GetById(Guid id);
 	}
 }
